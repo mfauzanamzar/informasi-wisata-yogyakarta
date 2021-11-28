fa50694fb733e63a83a2a9080f887c7a2958f6d8
Meilika <meilika.dwi101619@students.unila.ac.id>
2021-11-28 23:58:29 +0700

    Create Footer Component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import ListWisata from './pages/ListWisata';
+import Footer from './components/Footer';
 
 function App() {  
   return (
     <Router>
     <Switch>
-      <Route path="/" exact component={Home}/>
-      <Route path="/wisata" component={ListWisata} />
+      {/* <Route path="/" exact component={Home}/>
+      <Route path="/wisata" component={ListWisata}/> */}
     </Switch>
+    <Footer/>
   </Router>
   );
 }
