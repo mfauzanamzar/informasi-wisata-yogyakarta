99d29f377dda916dbb891737e71787f94554be14
Meilika <meilika.dwi101619@students.unila.ac.id>
2021-12-02 14:02:36 +0700

    Create ListWisata Page Content

--- src/pages/ListWisata.js
+++ src/pages/ListWisata.js
@@ -1,10 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import HeroListWisata from "../components/HeroListWisata";
+import {usePosts} from "../custom-hooks";
+import "./ListWisata.css";
 
 const ListWisata = () => {
+    const [posts, isLoading] = usePosts();
+    console.log(posts)
+
+    if(isLoading) return(
+        <h1>Loading</h1>
+    )
     return (
-        <div>
+        <div className="content-wisata">
             <HeroListWisata/>
+            <h1>Explore Wisata </h1>
+            <div className="wisata">
+                {posts.map((post) =>{
+                    return(
+                        <div className="wisata-item">
+                        <div className="list-wisata">
+                            <img className="wisata-pic" src={post.fields.image.fields.file.url} alt={post.fields.name}/>
+                            <Link className="wisata-name" key={post.fields.slug} to={post.fields.slug}>
+                                <p>{post.fields.name}</p>
+                            </Link>
+                        </div>
+                    </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
