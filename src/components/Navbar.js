e632639d2acf276a938759ee8e9e582611f2ec1d
mfauzanamzar <mfauzanamza@gmail.com>
2021-12-02 18:07:15 +0800

    menambahkan style navbar mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,50 @@
 import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import './Navbar.css'
 
 const Navbar = () => {
+
+    const [open, setOpen] = useState(false)
+
     return (
         <nav>
             <div className="navbar">
                 <div className="navbar__logo">
                     <h1>Logo</h1>
                 </div>
-                <ul className="navbar__lists">
-                    <li><a>Home</a></li>
-                    <li><a href="">Wisata</a></li>
-                    <li><a href="">Peta</a></li>
+                <ul className="navbar__lists nav-hidden">
+                    <Link to="/">
+                        <li>Home</li>
+                    </Link>
+                    <Link to="/wisata">
+                        <li>Wisata</li>
+                    </Link>
+                    <Link to="/Maps">
+                        <li>Maps</li>
+                    </Link>
                 </ul>
                 <div className="navbar-mobile">
-                    <i class="fa fa-bars"></i>
+                    <a href="#" onClick={() => setOpen(!open)}>
+                        <i class="fa fa-bars"></i>
+                    </a>
                 </div>
             </div>
+            {open &&
+                <div className="dropdown">
+                    <ul className="navbar__lists">
+                        <Link to="/">
+                            <li>Home</li>
+                        </Link>
+                        <Link to="/wisata">
+                            <li>Wisata</li>
+                        </Link>
+                        <Link to="/Maps">
+                            <li>Maps</li>
+                        </Link>
+                    </ul>
+                </div>
+            }
         </nav>
     )
 }
