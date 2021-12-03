472e75ce845a71ca2b2a97038a1926a5d1be30c5
mfauzanamzar <mfauzanamzar@gmail.com>
2021-12-03 20:06:44 +0800

    menampilkan data contentful ke page Detail dan menginstall react-leaflet

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,14 +1,30 @@
 import React from 'react'
-import {useParams} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import useSinglePost from '../custom-hooks/useSinglePost'
+import './Detail.css'
 
 const Detail = () => {
     const { slug } = useParams();
     const [post, isLoading] = useSinglePost(slug)
     console.log(post)
+
+    if (isLoading)
+        return (
+            <h1>Loading...</h1>
+        )
+
     return (
-        <div>
-            Halaman Detail
+        <div className="detail">
+            <h1 className="detail__title">{post.name}</h1>
+            <div className="detail__content">
+                <div className="content__image">
+                    <img src={post.image.fields.file.url} alt={post.name} />
+                </div>
+                <div className="content__description">{post.description}</div>
+            </div>
+            <div className="detail__map">
+                <h1>Location</h1>
+            </div>
         </div>
     )
 }
